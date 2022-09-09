f3009557f41efd5d820c31e0cdd751bf7147bd67
pblgllgs <pbl.gllgs@gmail.com>
2022-09-09 11:34:01 -0300

    about section complete

---
+++ src/components/NavigationDots.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const NavigationDots = ({ active }) => {
+  return (
+    <div className="app__navigation">
+      {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map(
+        (item, idx) => {
+          return (
+            <a
+              href={`#${item}`}
+              key={item + idx}
+              className="app__navigation-dot"
+              style={active === item ? { backgroundColor: '#313BAC' } : {}}
+            />
+          );
+        }
+      )}
+    </div>
+  );
+};
+
+export default NavigationDots;
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './About.scss';
+import { motion } from 'framer-motion';
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 
 const About = () => {
-  return <div>About</div>;
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query).then((data) => setAbouts(data));
+  });
+
+  return (
+    <>
+      <h2 className="head-text">
+        Yo se que un <span>Buen developer</span> <br /> significa un{' '}
+        <span>Buen negocio</span>{' '}
+      </h2>
+      <div className="app__profiles">
+        {abouts.map((about, idx) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={idx}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default About;
+export default AppWrap(About, 'about');
